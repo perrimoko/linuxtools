0c4ce8465cd2c070898b73876c34e2efda5cee72
perrimoko <96427935+perrimoko@users.noreply.github.com>
2021-12-22 09:43:17 -0400

    adsada

---
+++ bspwm.sh
@@ -0,0 +1,15 @@
+
+
+sudo apt install git
+sudo apt install -y tilix
+sudo apt install -y pcmanfm
+sudo apt install -y feh
+sudo apt install -y compton
+sudo apt install -y nitrogen
+sudo apt install -y lxappearance
+sudo apt install -y polybar
+git clone https://github.com/callmezatiel/olddotfiles
+sudo apt install -y bspwm
+
+
+
